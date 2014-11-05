d903fd853ec86b702dce6eac430411d1e4f8bab1
Yachida <august8still8sunburst@gmail.com>
2014-11-06 00:26:05 +0900

    refactoring.

--- lib.js
+++ lib.js
@@ -1,19 +1,9 @@
 function padZero(n) {
   return (n < 10) ? '0'+n : n;
 }
-function uniqueRandom(n, m){
-  var result = Math.floor((Math.random()*m) + 1);
-  while(n === result){
-    result = Math.floor((Math.random()*m) + 1);
-  }
-  return result;
-}
 
 function getRandom(n){
-  console.log(n);
-  var result = Math.floor((Math.random()*n));
-  console.log(result);
-  return result;
+  return Math.floor((Math.random()*n));
 }
 
 function getArray(count){
