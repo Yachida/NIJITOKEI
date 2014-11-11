a716941b6f6d232b826a05c9e939d93144b8db6b
Yachida <august8still8sunburst@gmail.com>
2014-11-12 00:04:58 +0900

    implement of valid img.

--- lib.js
+++ lib.js
@@ -8,14 +8,6 @@ function getRandom(n){
   return Math.floor((Math.random()*n));
 }
 
-function getArray(count){
-  var tmpArr = [];
-	for (var i = 0; i < count; i++) {
-		tmpArr.push( i+1 );
-	}
-	return tmpArr
-}
-
 function seekSize(src,dst){
   return (src / 2) + ((src / 2) * dst /100 );
 }
