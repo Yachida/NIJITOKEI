c512d88f15d98702885c6c6d843dfd954059505f
Yachida <august8still8sunburst@gmail.com>
2014-11-23 01:36:22 +0900

    use underscorejs.

--- lib.js
+++ lib.js
@@ -4,18 +4,6 @@ function padZero(n) {
   return (n < 10) ? '0'+n : n;
 }
 
-function getRandom(n){
-  return Math.floor((Math.random()*n));
-}
-
 function seekSize(src,dst){
   return Math.round((src / 2) + ((src / 2) * dst /100 ));
-}
-
-function arrayCopy(src){
-  var dst = [];
-  for(var d in src){
-    dst[dst.length] = src[d];
-  }
-  return dst;
 }
